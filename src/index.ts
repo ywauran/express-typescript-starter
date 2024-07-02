91645a20999d2ef626a06b222da9ea41acd8f3e2
Yohanes Harke Wauran <harkewauran23@gmail.com>
2024-07-02 14:15:57 +0800

    change config

--- src/index.ts
+++ src/index.ts
@@ -1,41 +1,41 @@
-import express, { Express, Request, Response } from 'express'
-import cors from 'cors'
-import helmet from 'helmet'
-import 'dotenv/config'
+import express, { Express, Request, Response } from "express";
+import cors from "cors";
+import helmet from "helmet";
+import "dotenv/config";
 
-import * as middleware from './middleware'
+import * as middleware from "./middleware";
 
-import articlesRouter from './routers/articles.router'
+import articlesRouter from "./routers/articles.router";
 
-const PORT = process.env.PORT || 8080
-const ENV = process.env.NODE_ENV || 'production'
+const PORT = process.env.PORT || 8080;
+const ENV = process.env.NODE_ENV || "production";
 
-const app: Express = express()
+const app: Express = express();
 
-app.use(helmet())
+app.use(helmet());
 
-app.use(cors())
+app.use(cors());
 
-app.use(express.json())
+app.use(express.json());
 
-app.use(middleware.httpLogger)
+app.use(middleware.httpLogger);
 
-app.get('/', (req: Request, res: Response) => {
-  res.status(200).send('Welcome')
-})
+app.get("/", (req: Request, res: Response) => {
+  res.status(200).send("Welcome");
+});
 
 // Articles routes
 
-app.use('/articles', articlesRouter)
+app.use("/articles", articlesRouter);
 
 // Error hanlding middleware
 
-app.use(middleware.errorHandler)
+app.use(middleware.errorHandler);
 
-app.use(middleware.notFoundHandler)
+app.use(middleware.notFoundHandler);
 
 const server = app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT} in ${ENV} environment`)
-})
+  console.log(`Server running on port ${PORT} in ${ENV} environment`);
+});
 
-export { app as default, server }
+export { app as default, server };
